a8c5ab71f792552b11f961ac29fcbc0c16fa8be1
Alan Lu <alanlu1023@gmail.com>
2018-10-19 15:10:25 -0500

    Added build artifacts and migrations to package

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    mainnet: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1"
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3"
+    },
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4"
+    },
+  }
+};
