2aee7da24682552439650d5f55d34cfe27be4da9
Victor Porton <porton@narod.ru>
2020-12-10 03:26:05 +0200

    Solidity 0.7.1 support

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,10 +1,11 @@
-pragma solidity >=0.4.22 <0.6;
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.7.0 <0.8;
 
 contract Migrations {
     address public owner;
     uint public lastCompletedMigration;
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
 
